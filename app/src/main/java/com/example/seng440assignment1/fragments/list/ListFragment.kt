b41fd59a9490c43cd12da01300f7336445f380e4
David Turton <djt112@uclive.ac.nz>
2021-04-06 15:52:24 +1200

    Added new functionality to make app set implicit intent for a reminder on the calendar app.

--- app/src/main/java/com/example/seng440assignment1/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/seng440assignment1/fragments/list/ListFragment.kt
@@ -1,6 +1,9 @@
 package com.example.seng440assignment1.fragments.list
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.CalendarContract
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +11,9 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.seng440assignment1.R
 import kotlinx.android.synthetic.main.fragment_list.view.*
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 class ListFragment : Fragment() {
@@ -31,6 +37,30 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_graphFragment)
         }
 
+        view.emailButton.setOnClickListener {
+            sendIntent()
+        }
+
         return view
     }
+
+    fun sendIntent(){
+        var calendar = Calendar.getInstance()
+        val intent = Intent(Intent.ACTION_INSERT, CalendarContract.Events.CONTENT_URI).apply {
+            val beginTime: Calendar = Calendar.getInstance().apply {
+                set(Calendar.getInstance().get(Calendar.YEAR), Calendar.getInstance().get(Calendar.MONTH), Calendar.getInstance().get(Calendar.DAY_OF_MONTH) + 1, 12, 0)
+            }
+            val endTime = Calendar.getInstance().apply {
+                set(Calendar.getInstance().get(Calendar.YEAR), Calendar.getInstance().get(Calendar.MONTH), Calendar.getInstance().get(Calendar.DAY_OF_MONTH) + 1, 13, 0)
+            }
+            putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.timeInMillis)
+            putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.timeInMillis)
+            putExtra(CalendarContract.Events.TITLE, "Use Health Visualiser")
+            putExtra(CalendarContract.Events.EVENT_LOCATION, "On your phone")
+        }
+        startActivity(intent)
+
+    }
+
+
 }
